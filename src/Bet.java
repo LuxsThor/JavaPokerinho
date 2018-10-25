78d9d65e5e7d8e9be334ce5fa3f89d39b7dbad72
LuxsThor <lucas.thorbecke@bluewin.ch>
2018-10-25 10:22:14 +0200

    added bet class

--- src/Bet.java
+++ src/Bet.java
@@ -1,4 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+
 public class Bet {
+    //class and object variables
+    private int bet;
+
+    //constructors
+    public Bet(){
+        bet = 0;
+    }
+
+    public Bet(int bet){
+        this.bet = bet;
+    }
+
+    // getters and setters
+    public int getBet(){
+        return bet;
+    }
+
+    public void setBet(int bet){
+        this.bet = bet;
+    }
+
+    //some testing action
+    public static void main(String[] args) {
+        Scanner testinput = new Scanner(System.in);
+        Bet testbet = new Bet();
+        System.out.println("the testbet value is "+ testbet.getBet());
+        testbet.setBet(5);
+        System.out.println("after setting the bet to 5 it is: " + testbet.getBet());
+        Bet anothertestbet = new Bet(6);
+        System.out.println("this bets value should already be set to 6 and its value is: " + anothertestbet.getBet());
+    }
 }
 
 
